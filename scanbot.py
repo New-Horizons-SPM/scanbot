f7ec3fa2d9412c0b823d56bc65d3e73828adba42
ceds92 <julian.ceddia@monash.edu>
2022-05-05 16:30:18 +1000

    Convert path object to string
    pass the path string into blob.upload_from_filename instead of the path object

--- scanbot.py
+++ scanbot.py
@@ -98,7 +98,7 @@ class ScanBot(object):
 
             bucket = storage.bucket()
             blob   = bucket.blob('scanbot/' + filename)
-            blob.upload_from_filename(path)
+            blob.upload_from_filename(str(path))
 
             url = blob.generate_signed_url(expiration=9000000000)
             bot_handler.send_reply(message, url)

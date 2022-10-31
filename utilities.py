e3c372fbcf75bfc6fa2b56d1bf49db2cb0916419
ceds92 <julian.ceddia@monash.edu>
2022-10-31 17:25:14 +1100

    Add the classify utility where images can be classified.
    Implemented the ability to detect the number of tip changes
    
    ToDo:
     - Tip sharpness
     - Close-range double
     - Long-range double
     - Rough/unscannable area
     - Run the same algorithms on partial images

--- utilities.py
+++ utilities.py
@@ -10,7 +10,8 @@ import pickle
 import math
 import numpy as np
 import scipy.signal as sp
-from PIL import Image
+import nanonispyfit as napfit
+import cv2
 
 def pklDict(scanData,filePath,x,y,w,h,angle,pixels,lines,comments=""):
     filename = ntpath.split(filePath)[1]
@@ -101,3 +102,74 @@ def makeGif(GIF):
 
     """
     pass
+###############################################################################
+# Make gif
+###############################################################################
+def classify(data,tipChanges=True,sharpness=False,closeDouble=False,longDouble=False):
+    """
+    This checks a raw scan for the following:
+
+    Parameters
+    ----------
+    scanData    : Raw scan data
+    tipChanges  : Look for tip changes.
+    sharpness   : Quantify sharpness.
+    closeDouble : Look for double tip (close range)
+    longDouble  : Look for double tip (long range)
+
+    Returns
+    -------
+    dict:
+    "tipChanges"   : number of detected tip changes
+    "sharpness"    : a sharpness score from zero to 10  (not implemented yet)
+    "closeDouble"  : 0 = not doubled. 1 = doubled       (not implemented yet)
+    "longDouble"   : 0 = not doubled. 1 = doubled       (not implemented yet)
+    "nan"          : data passed in contains NANs
+
+    """
+    nan = np.isnan(data).any()                                                  # Flag to say there are nans in data which might affect  analysis
+    scanData = np.nan_to_num(data)
+    tipChangeCount = findTipChanges(scanData.copy())
+
+    return {"tipChanges" : tipChangeCount,
+            "nan"        : nan}
+
+def findTipChanges(scanData):
+    scanData = np.diff(scanData,axis=0)**2                                      # Take the derivative in y to enhance tip changes that occur as horizontal lines. **2 to further enhance
+    scanData /= np.max(abs(scanData))                                           # Normalise
+    vmin, vmax = napfit.filter_sigma(scanData)                                  # Find 3 sigma
+    scanData -= vmin                                                            # Subtract the minimum
+    scanData /= abs(vmax - vmin)                                                # And divide by the range (3 sigma)
+    scanData[scanData > 1] = 1                                                  # Saturate everything over 1
+    scanData *= 255                                                             # Change the range so it's uint8 representable
+    grey = scanData.astype(np.uint8)                                            # Change the data type to uint8 for the Canny filter
+    
+    edges = cv2.Canny(grey, threshold1=50, threshold2=150, apertureSize = 3)    # Edge detection
+    lines = cv2.HoughLinesP(image=edges, rho=1, theta=np.pi/180, threshold=1,   # Pull lines from edges
+                            lines=np.array([]),minLineLength=1,maxLineGap=0)
+    
+    if(type(lines) == type(None)): lines = []                                   # Make it an empty list if there are no lines detected
+    
+    lineDict = {}                                                               # Dictionary to sort all lines according to their y-coodinate
+    for line in lines:                                                          
+        if(abs(line[0][1] == line[0][3])):                                      # Only process this line if it is horizontal (i.e. y1=y2)
+            if(line[0][1] in lineDict):                                         # If there's already a horizontal line at this y-coordinate
+                lineDict[line[0][1]].append(line)                               # Add it to the dictionary
+            else:                                                               # If this is the first horizontal line at this y-coordinate...
+                lineDict[line[0][1]] = [line]                                   # Create a new dictionary entry with the y-coordinate as the key and add the line
+
+    tipChanges = []                                                             # This will be the list of y-coordinates where tip changes have occurred
+    minLineLength=len(grey[0])/8                                                # Only count lines if they are at least 1/8 of the image in length
+    for y,lines in lineDict.items():                                            # For each y-coordinate we've found lines at
+        totalLength = 0
+        for line in lines:
+            totalLength += line[0][2] - line[0][0]                              # Accumulate total line length at this y coordinate.
+        if(totalLength > minLineLength):                                        # If the total length is greater than our threshold
+            tipChanges.append(y)                                                # Count this line as a tip change and append its y coord
+
+    tipChanges = np.sort(tipChanges)                                            # Sort coords by ascending order
+    diff = np.diff(tipChanges)                                                  # Group lines that are less than two pixels apart in y and
+    tipChangeCount = sum(diff > 2)                                              # Count the number of tip changes to be the number of groups
+    
+    return tipChangeCount
+    

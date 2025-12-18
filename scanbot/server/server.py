4280107dd767db23a1977d7d5d195a81c5f5a29b
Julian Ceddia <jdceddia@gmail.com>
2025-12-18 16:13:14 +1100

    Update version number in server.py

--- scanbot/server/server.py
+++ scanbot/server/server.py
@@ -355,7 +355,7 @@ def getLatestVersion(url):
 
 @app.route('/check_updates')
 def check_updates():
-    currentVersion = '4.5.1'
+    currentVersion = '4.5.2'
     latestVersion  = getLatestVersion('https://us-central1-scanbot-46390.cloudfunctions.net/getVersion')
 
     update = False
